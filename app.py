33d3407ceb969695452256336fa720038779a7f1
kumar poudel <kumarpoudel@kumars-MBP.lan>
2023-08-04 20:50:50 -0500

    app.py typo error

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
 import os
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
 def hello():
-    return "Hello World!"
+    return 'I am good, how about you?'
 
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)

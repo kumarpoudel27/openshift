eeee3a82d6885f04d60a5527b6b88e4433425165
kumar poudel <kumarpoudel@kumars-MBP.lan>
2023-08-04 19:13:29 -0500

    app.py type error

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+from flask import Flask
 import os
-from flask import flask 
+
 app = Flask(__name__)
-@app.route("/")
-def main():
-    return "WELCOME"
 
-@app.route('/how are you')
+@app.route('/')
 def hello():
-    return 'i am good'
+    return "Hello World!"
 
-if __name__=="__main__"
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
